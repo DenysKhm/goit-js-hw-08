27902f59b3c47758dae10ac919a1b2e85b8a4462
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-08-04 18:40:04 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this linenpi
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryMarkup();
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+function galleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a 
+    class="gallery__item" 
+    href=${original}>
+  <img 
+  class="gallery__image" 
+  src= ${preview}
+  alt=${description} />
+</a>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
