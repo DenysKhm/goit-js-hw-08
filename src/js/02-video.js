a2a06afc9dc23f44a81fdfdb128dfc73cb1b16c5
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-08-05 18:58:28 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(Number(currentTime));
+
+console.log(typeof currentTime);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
