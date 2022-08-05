dab5603e8d0047a9ac5acaf71a99dc48e83a559f
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-08-05 20:56:41 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('input', throttle(savedStorage, 500));
+
+function savedStorage() {
+  const { email, message } = feedbackForm.elements;
+  const objJson = JSON.stringify({
+    email: email.value,
+    message: message.value,
+  });
+  localStorage.setItem('feedback-form-state', objJson);
+  console.log(`email: ${email.value},message: ${message.value}`);
+}
+
+const savedForm = localStorage.getItem('feedback-form-state');
+if (savedForm) {
+  const newObjInfo = JSON.parse(savedForm);
+  const { email, message } = feedbackForm.elements;
+  email.value = newObjInfo.email;
+  message.value = newObjInfo.message;
+}
+
+feedbackForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  const { email, message } = feedbackForm.elements;
+  if (email.value !== '' && message.value !== '') {
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+}
